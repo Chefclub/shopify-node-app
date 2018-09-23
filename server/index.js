6c2c820e272de46528d5011f915e55fd581a6148
medhabibdhif <mohamed-habib.dhif@imt-atlantique.net>
2018-09-23 17:09:48 +0300

    add customer scopes

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const shopifyConfig = {
   host: SHOPIFY_APP_HOST,
   apiKey: SHOPIFY_APP_KEY,
   secret: SHOPIFY_APP_SECRET,
-  scope: ['write_fulfillments, read_fulfillments,read_shipping,read_all_orders,write_shipping,read_orders, write_orders, write_products'],
+  scope: ['write_fulfillments, read_fulfillments,read_shipping,read_all_orders,write_shipping,read_customers,write_customers,read_orders,write_orders,write_products'],
   shopStore: new MemoryStrategy(),
   afterAuth(request, response) {
     if (request) {
