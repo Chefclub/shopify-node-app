f81a71fe7c5d810ea6e5108b28aa090e185b5241
habibdhif <habib.dhif@gmail.com>
2020-10-09 15:22:18 +0200

    Add write reports scope

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const shopifyConfig = {
   host: SHOPIFY_APP_HOST,
   apiKey: SHOPIFY_APP_KEY,
   secret: SHOPIFY_APP_SECRET,
-  scope: ['read_reports, write_fulfillments, read_fulfillments,read_shipping,read_all_orders,write_shipping,read_customers,read_orders,write_orders,write_products'],
+  scope: ['write_reports, write_fulfillments, read_fulfillments,read_shipping,read_all_orders,write_shipping,read_customers,read_orders,write_orders,write_products'],
   shopStore: new MemoryStrategy(),
   afterAuth(request, response) {
     if (request) {
